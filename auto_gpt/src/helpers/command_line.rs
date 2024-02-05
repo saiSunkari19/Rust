25c58ccbb4ba4e2dd35a3f6f343972c88667324b
saisunkari19 <saisunkari19@protonmail.com>
2024-02-05 13:51:55 +0530

    adding print colors

--- auto_gpt/src/helpers/command_line.rs
+++ auto_gpt/src/helpers/command_line.rs
@@ -5,6 +5,37 @@ use crossterm::{
 
 use std::io::{stdin, stdout};
 
+
+pub enum PrintCommand  {
+    AICall, 
+    UnitTest,
+    Issue
+}
+
+
+impl PrintCommand {
+    pub fn print_agent_message(&self, agent_pos : &str, agent_statment: &str){
+        let mut stdout = stdout();
+
+        let statement_color = match self {
+            Self::AICall => Color::Cyan,
+            Self::UnitTest => Color::Magenta,
+            Self::Issue => Color::Red,
+        };
+
+        stdout.execute(SetForegroundColor(Color::Green)).unwrap();
+        print!("Agent: {} ", agent_pos);
+
+        stdout.execute(SetForegroundColor(statement_color)).unwrap();
+        println!("{}", agent_statment);
+
+        // Reset Color 
+        stdout.execute(ResetColor).unwrap();
+
+
+    }
+}
+
 pub fn get_user_response(question: &str) -> String {
     let mut stdout = stdout();
 
@@ -18,4 +49,18 @@ pub fn get_user_response(question: &str) -> String {
     stdin().read_line(&mut user_response);
 
     return user_response.trim().to_string();
+}
+
+
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_print_agent_message(){
+        PrintCommand::AICall.
+        print_agent_message("Managaing", "Testing testing");
+    }
 }
